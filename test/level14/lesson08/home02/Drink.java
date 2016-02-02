05551508ca4a7dc7eee8efc835281bf654a0e31b
Anna <d34d4l1c3@gmail.com>
2016-02-02 16:26:57 +0300

    NORMAL

---
+++ test/level14/lesson08/home02/Drink.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level14.lesson08.home02;
+
+/**
+ * Created by Анна on 07.10.2015.
+ */
+public abstract class Drink
+{
+    public void taste()
+    {
+        System.out.println("Вкусно");
+    }
+	System.out.println("djahskjQHSKJ");
+}
